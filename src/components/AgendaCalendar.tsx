b06e25a01676468ea54f012f6e21aed64cd09c0d
Daniel Bocachica <dbocachica@unimagdalena.edu.co>
2026-07-28 21:24:08 -0500

    fix: Resolve date-fns import error for v3

--- src/components/AgendaCalendar.tsx
+++ src/components/AgendaCalendar.tsx
@@ -1,11 +1,8 @@
-﻿"use client";
+"use client";
 import React, { useState, useEffect } from 'react';
 import { Calendar, dateFnsLocalizer, View, Views } from 'react-big-calendar';
-import format from 'date-fns/format';
-import parse from 'date-fns/parse';
-import startOfWeek from 'date-fns/startOfWeek';
-import getDay from 'date-fns/getDay';
-import es from 'date-fns/locale/es';
+import { format, parse, startOfWeek, getDay } from 'date-fns';
+import { es } from 'date-fns/locale/es';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import '../app/agenda/calendar.css';
 import { supabase } from '@/lib/supabase';
